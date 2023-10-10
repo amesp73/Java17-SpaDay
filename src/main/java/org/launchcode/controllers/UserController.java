41f16f7d33697c003b8e50c53e2bd6a41bdbc2f7
amesp73 <amitrpatel502@gmail.com>
2023-10-09 20:18:29 -0500

    studio

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,6 +1,8 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
+import org.springframework.validation.Errors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,17 +12,23 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
+    public String processAddUserForm(@ModelAttribute @Valid User user, Model model, Error errors) {
+//        model.addAttribute("user", user);
+//        model.addAttribute("verify", verify);
+//        model.addAttribute("username", user.getUsername());
+//        model.addAttribute("email", user.getEmail());
+
+        System.out.println(errors.hasErrors());
+        if(errors.hasErrors()) {
+            return "user/add";
+        }
+        if (user.getPassword().equals(user.getVerifyPassword())) {
             return "user/index";
         }
         else {
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,19 +1,38 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @Size(min = 5, max = 15, message = "Username has to be between 5-15 characters)")
     private String username;
+    @Email(message = "Invalid email format")
     private String email;
+    @Size(min = 6)
+    @NotBlank(message = "Password must be at least 6 characters long")
     private String password;
+    @Size(min = 6)
+    @NotBlank(message = "Password must be at least 6 characters long")
+    private String verifyPassword;
 
     public User() {
 
     }
-
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
     }
 
     public String getUsername() {
@@ -38,6 +57,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+
+    private void checkPassword() {
+        if(this.password != null && !password.equals(verifyPassword)) {
+            setVerifyPassword(null);
+        }
     }
 }
 
